3165375413b7a84bbe2cc5527c175a10fcf41705
alachaud <antoine.lachaud91620@gmail.com>
2021-11-11 14:22:39 +0100

    Première version de l'ajout du graphe en mémoire

--- src/Graphe.java
+++ src/Graphe.java
@@ -1,3 +1,56 @@
-public class Graphe {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Graphe {
+    private int nbSommets;
+    private int nbArcs;
+    private ArrayList<Integer[]> data; //Tableau contenant des tableaux contenant chacun [extremite initiale, terminale, valeur arc]
+
+    public Graphe(int nbSommets, int nbArcs) {
+        this.nbSommets = nbSommets;
+        this.nbArcs = nbArcs;
+    }
+
+    //Constructeur du graphe
+    public Graphe(int nbSommets, int nbArcs, ArrayList<Integer[]> data) {
+        this.nbSommets = nbSommets;
+        this.nbArcs = nbArcs;
+        this.data = data;
+    }
+
+    //Affichage du graphe de manière brute (utilisée à des fins de tests)
+    public void afficherBrut() {
+        System.out.println("GRAPHE BRUT");
+        System.out.println("- - - - -");
+        System.out.println(nbSommets);
+        System.out.println(nbArcs);
+
+        for(Integer[] lineData: data) {
+            System.out.println(Arrays.toString(lineData));
+        }
+    }
+
+    //GETTER SETTER NBSOMMETS
+    public int getNbSommets() {
+        return nbSommets;
+    }
+    public void setNbSommets(int nbSommets) {
+        this.nbSommets = nbSommets;
+    }
+
+    //GETTER SETTER NBARCS
+    public int getNbArcs() {
+        return nbArcs;
+    }
+    public void setNbArcs(int nbArcs) {
+        this.nbArcs = nbArcs;
+    }
+
+    //GETTER SETTER DATA
+    public ArrayList<Integer[]> getData() {
+        return data;
+    }
+    public void setData(ArrayList<Integer[]> data) {
+        this.data = data;
+    }
 }
--- src/TestGraphe.java
+++ src/TestGraphe.java
@@ -1,5 +1,94 @@
-public class TestGraphe {
-    public static void main(String[] args) {
+import java.io.*;
+import java.lang.reflect.Array;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
+public class TestGraphe {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Bienvenue dans le programme de test de l'algorithme de Floyd-Warshall !");
+
+        // 1.Choix du graphe
+        Scanner sc = new Scanner(System.in); // Création d'un scanner pour que l'utilisateur entre le numéro du graphe
+
+        System.out.println("Veuillez entrer le numéro du graphe que vous voulez analyser: ");
+        int numGraphe = sc.nextInt(); // L'utilisateur entre le numéro
+
+        // 2.Lecture du graphe sur le fichier et mise en mémoire
+        String path = "Graphes/" + numGraphe + ".txt"; //Chemin vers le graphe mentionné par l'utilisateur
+        List<String> content = Files.readAllLines(Paths.get(path));
+
+        int nbSommets = Integer.parseInt(content.get(0)); //1ère ligne = nbSommets, donc on l'assigne
+        int nbArcs = Integer.parseInt(content.get(1)); //2ème ligne = nbArcs
+
+        String[] lineDataString; //Données de la ligne au format String
+        Integer[] lineData = {null, null, null};
+        int lineDataIndex; //Donnée concernée (Extrémité Initiale, terminale, ou valeur de l'arc
+        ArrayList<Integer[]> data = new ArrayList<>(); //ArrayList contenant les données de chaque ligne
+
+        for(int i = 2; i < content.size(); i++) {
+            lineDataString = content.get(i).split(" "); //Convertion de la ligne en tableau contenant les données de la ligne
+
+            for(int j = 0; j < lineDataString.length; j++) {
+                lineDataIndex = Integer.parseInt(lineDataString[j]); //String to int
+                lineData[j] = lineDataIndex; //Ajout de la donnée de la ligne
+            }
+
+            data.add(lineData);
+        }
+
+        Graphe graphe = new Graphe(nbSommets, nbArcs, data);
+        graphe.afficherBrut();
+
+        //BufferedReader permet de lire le contenu d'un fichier
+//        try (BufferedReader br = new BufferedReader(new FileReader(grapheFile))) {
+//            StringBuilder sb = new StringBuilder(); //Utiliser pour mettre en mémoire le contenu du fichier
+//
+//            String line = br.readLine(); //Lit la première ligne
+//            int nbSommets = Integer.parseInt(line); //1ère ligne = nbSommets, donc on l'assigne
+//            line = br.readLine(); //Lit la deuxième ligne
+//            int nbArcs = Integer.parseInt(line); //2ème ligne = nbArcs
+//            Graphe g = new Graphe(nbSommets, nbArcs);
+//            System.out.println(g.getNbSommets());
+//            System.out.println(g.getNbArcs());
+//
+//            String[] lineDataString; //Données de la ligne au format String
+//            Integer[] lineData = { 0, 0, 0};
+//            int lineDataIndex; //Numéro de la donnée concernée (Extrémité Initiale, terminale, ou valeur de l'arc
+//            ArrayList<Integer[]> data = new ArrayList<>(); //ArrayList contenant les données de chaque ligne
+//
+//            //Tant que le BufferedReader ne renvoit pas nul (=tant qu'on est pas arrivés à la fin du fichier),
+//            //On lit et on met en mémoire le contenu du graphe dans un string "contenu"
+//            while (line != null) {
+//                sb.append(line);
+//                sb.append(System.lineSeparator());
+//                line = br.readLine();
+//                lineDataString = line.split(" "); //Convertion de la ligne en tableau contenant les données de la ligne
+//
+//                //Pour chaque donnée de ligne, l'ajouter au tableau de la ligne
+//                for(int i = 0; i < lineDataString.length; i++) {
+//                    lineDataIndex = Integer.parseInt(lineDataString[i]); //String to int
+//                    lineData[i] = lineDataIndex; //Ajout de la donnée de la ligne
+//                }
+//                data.add(lineData);
+//            }
+//
+//            Graphe graphe = new Graphe(nbSommets, nbArcs, data);
+//            graphe.afficherBrut();
+//            String contenu = sb.toString();
+//            System.out.println(contenu); //Affichage du contenu du fichier
+//        }
+//        catch (Exception e) { //Si le fichier n'est pas trouvé, on affiche une erreur
+//            System.out.println("[ERR] Fichier introuvable");
+//        }
+
+        // 3.Affichage du graphe (sous forme de matrice(s))
+        // 4.Floyd-Warshall
+        // 5.Existence de circuit absorbant ?
+            // 5.1.Si NON: affichage des chemins
+        //6.Demander si on veut recommencer avec un autre graphe
     }
 }
