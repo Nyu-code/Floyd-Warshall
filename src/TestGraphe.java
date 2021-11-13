1349e3ff73ca1bad3a8147b54bd86d9d7da75d2b
alachaud <antoine.lachaud91620@gmail.com>
2021-11-13 17:24:29 +0100

    Vérifie si le fichier demandé existe.

--- src/TestGraphe.java
+++ src/TestGraphe.java
@@ -16,37 +16,41 @@ public class TestGraphe {
 
         System.out.println("Veuillez entrer le numéro du graphe que vous voulez analyser: ");
         int numGraphe = sc.nextInt(); // L'utilisateur entre le numéro
-
-        // 2.Lecture du graphe sur le fichier et mise en mémoire
         String path = "Graphes/" + numGraphe + ".txt"; //Chemin vers le graphe mentionné par l'utilisateur
-        List<String> content = Files.readAllLines(Paths.get(path));
+        File f = new File(path);
+        if(!f.exists()) {
+            System.out.println("[ERR] Fichier introuvable.");
+        } else {
+            // 2.Lecture du graphe sur le fichier et mise en mémoire
+            List<String> content = Files.readAllLines(Paths.get(path));
 
-        int nbSommets = Integer.parseInt(content.get(0)); //1ère ligne = nbSommets, donc on l'assigne
-        int nbArcs = Integer.parseInt(content.get(1)); //2ème ligne = nbArcs
+            int nbSommets = Integer.parseInt(content.get(0)); //1ère ligne = nbSommets, donc on l'assigne
+            int nbArcs = Integer.parseInt(content.get(1)); //2ème ligne = nbArcs
 
-        String[] lineDataString; //Données de la ligne au format String
-        int lineDataIndex; //Donnée concernée (Extrémité Initiale, terminale, ou valeur de l'arc
-        ArrayList<ArrayList<Integer>> data = new ArrayList<>(); //ArrayList contenant les données de chaque ligne
+            String[] lineDataString; //Données de la ligne au format String
+            int lineDataIndex; //Donnée concernée (Extrémité Initiale, terminale, ou valeur de l'arc
+            ArrayList<ArrayList<Integer>> data = new ArrayList<>(); //ArrayList contenant les données de chaque ligne
 
-        for(int i = 2; i < content.size(); i++) {
-            ArrayList<Integer> lineData = new ArrayList<>(); //Ligne contenant les données
-            lineDataString = content.get(i).split(" "); //Convertion de la ligne en tableau contenant les données de la ligne
+            for(int i = 2; i < content.size(); i++) {
+                ArrayList<Integer> lineData = new ArrayList<>(); //Ligne contenant les données
+                lineDataString = content.get(i).split(" "); //Convertion de la ligne en tableau contenant les données de la ligne
 
-            for (String s : lineDataString) {
-                lineDataIndex = Integer.parseInt(s); //String to int
-                lineData.add(lineDataIndex); //Ajout de la donnée de la ligne
+                for (String s : lineDataString) {
+                    lineDataIndex = Integer.parseInt(s); //String to int
+                    lineData.add(lineDataIndex); //Ajout de la donnée de la ligne
+                }
+
+                data.add(lineData); //Ajout de la ligne à l'ArrayList générale
             }
 
-            data.add(lineData); //Ajout de la ligne à l'ArrayList générale
-        }
+            Graphe graphe = new Graphe(nbSommets, nbArcs, data); //Création d'un nouveau graphe contenant les valeurs brutes
+            graphe.afficherBrut();
 
-        Graphe graphe = new Graphe(nbSommets, nbArcs, data); //Création d'un nouveau graphe contenant les valeurs brutes
-        graphe.afficherBrut();
-
-        // 3.Affichage du graphe (sous forme de matrice(s))
-        // 4.Floyd-Warshall
-        // 5.Existence de circuit absorbant ?
+            // 3.Affichage du graphe (sous forme de matrice(s))
+            // 4.Floyd-Warshall
+            // 5.Existence de circuit absorbant ?
             // 5.1.Si NON: affichage des chemins
-        //6.Demander si on veut recommencer avec un autre graphe
+            //6.Demander si on veut recommencer avec un autre graphe
+        }
     }
 }
